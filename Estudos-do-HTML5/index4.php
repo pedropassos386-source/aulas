80435dcf3fe253e473a3dc95f324157eb559fd6b
pedropassos386-source <pedropassos386@gmail.com>
2026-05-27 16:48:49 -0300

    fazendo tela de login

---
+++ Estudos-do-HTML5/index4.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>Login Facebook</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <meta name="description" content="login">
+        <meta name="keywords" content="login">
+        <meta name="author" content="Pedro Passos">
+        <link rel="stylesheet" href="css/style4.css">
+    </head>
+    <body>
+    <header>
+        <div class="center">
+            <div class="logo">
+                <h2>Vought International</h2>
+            </div>
+            <form method="post" class="form-login">
+                <div class="form-element">
+                    <input placeholder="Email ou Telefone" type="email"/>
+                </div><!--Form-element-->
+                <div class="form-element">
+                    <input placeholder="Digite sua Senha" type="Password" id="senha"/>
+                </div><!--Form-element-->
+                <div class="form-element">
+                    <input type="submit" name="ação" value="Enviar"/>
+                </div><!--Form-element-->
+            </form><!--Form-Login-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </header>
+    </body>
+    
+</html>
